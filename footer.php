4f760557e080c637f4816896f6a00cd44f7a14d2
Misa Cvetkovic <misa.cvetkovic01@gmail.com>
2016-06-14 00:24:09 +0200

    add footer.php, functions.php, header.php and sidebar.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Hyperion
+ */
+
+?>
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
 
+
+
+<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
